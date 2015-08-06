35b0b545257e0bec80961302af21b20f00d5d5d2
Jordi Cenzano <jcenzano@brightcove.com>
2015-08-06 15:16:26 -0700

    Changed auto generated files from 10h to 30h

--- scripts/rtmploadtest.sh
+++ scripts/rtmploadtest.sh
@@ -16,19 +16,19 @@ TEST_MEDIA_PATH="/data/test_material/${FILE_TO_STREAM}"
 FFMPEGOPTS="-re -y -i ${TEST_MEDIA_PATH} -vcodec copy -acodec copy -f flv"
 
 if [ "$FILE_TO_STREAM" = "auto720p30@2M" ]; then
-	FFMPEGOPTS='-f lavfi -re -i testsrc=duration=36000:size=1280x720:rate=30 -f lavfi -re -i sine=frequency=1000:duration=36000:sample_rate=44100 -pix_fmt yuv420p -c:v libx264 -b:v 2000k -g 30 -profile:v baseline -preset veryfast -c:a libfdk_aac -b:a 96k -f flv'
+	FFMPEGOPTS='-f lavfi -re -i testsrc=duration=108000:size=1280x720:rate=30 -f lavfi -re -i sine=frequency=1000:duration=108000:sample_rate=44100 -pix_fmt yuv420p -c:v libx264 -b:v 2000k -g 30 -profile:v baseline -preset veryfast -c:a libfdk_aac -b:a 96k -f flv'
 fi
 if [ "$FILE_TO_STREAM" = "auto720p60@3M" ]; then
-	FFMPEGOPTS='-f lavfi -re -i testsrc=duration=36000:size=1280x720:rate=60 -f lavfi -re -i sine=frequency=1000:duration=36000:sample_rate=44100 -pix_fmt yuv420p -c:v libx264 -b:v 3000k -g 60 -profile:v baseline -preset veryfast -c:a libfdk_aac -b:a 96k -f flv'
+	FFMPEGOPTS='-f lavfi -re -i testsrc=duration=108000:size=1280x720:rate=60 -f lavfi -re -i sine=frequency=1000:duration=108000:sample_rate=44100 -pix_fmt yuv420p -c:v libx264 -b:v 3000k -g 60 -profile:v baseline -preset veryfast -c:a libfdk_aac -b:a 96k -f flv'
 fi
 if [ "$FILE_TO_STREAM" = "auto1080p30@3M" ]; then
-	FFMPEGOPTS='-f lavfi -re -i testsrc=duration=36000:size=1920x1080:rate=30 -f lavfi -re -i sine=frequency=1000:duration=36000:sample_rate=44100 -pix_fmt yuv420p -c:v libx264 -b:v 3000k -g 30 -profile:v baseline -preset veryfast -c:a libfdk_aac -b:a 96k -f flv'
+	FFMPEGOPTS='-f lavfi -re -i testsrc=duration=108000:size=1920x1080:rate=30 -f lavfi -re -i sine=frequency=1000:duration=108000:sample_rate=44100 -pix_fmt yuv420p -c:v libx264 -b:v 3000k -g 30 -profile:v baseline -preset veryfast -c:a libfdk_aac -b:a 96k -f flv'
 fi
 if [ "$FILE_TO_STREAM" = "auto1080p60@4M" ]; then
-	FFMPEGOPTS='-f lavfi -re -i testsrc=duration=36000:size=1920x1080:rate=60 -f lavfi -re -i sine=frequency=1000:duration=36000:sample_rate=44100 -pix_fmt yuv420p -c:v libx264 -b:v 4000k -g 60 -profile:v baseline -preset veryfast -c:a libfdk_aac -b:a 96k -f flv'
+	FFMPEGOPTS='-f lavfi -re -i testsrc=duration=108000:size=1920x1080:rate=60 -f lavfi -re -i sine=frequency=1000:duration=108000:sample_rate=44100 -pix_fmt yuv420p -c:v libx264 -b:v 4000k -g 60 -profile:v baseline -preset veryfast -c:a libfdk_aac -b:a 96k -f flv'
 fi
 if [ "$FILE_TO_STREAM" = "auto240p30@500k" ]; then
-	FFMPEGOPTS='-f lavfi -re -i testsrc=duration=36000:size=320x240:rate=30 -f lavfi -re -i sine=frequency=1000:duration=36000:sample_rate=44100 -pix_fmt yuv420p -c:v libx264 -b:v 500k -g 30 -profile:v baseline -preset veryfast -c:a libfdk_aac -b:a 96k -f flv'
+	FFMPEGOPTS='-f lavfi -re -i testsrc=duration=108000:size=320x240:rate=30 -f lavfi -re -i sine=frequency=1000:duration=108000:sample_rate=44100 -pix_fmt yuv420p -c:v libx264 -b:v 500k -g 30 -profile:v baseline -preset veryfast -c:a libfdk_aac -b:a 96k -f flv'
 fi
 
 DEST_URL="rtmp://${AUTH_STRING}${STREAMING_SERVER_AND_PORT}/${STREAMING_APP}/${STREAM_NAME}"
